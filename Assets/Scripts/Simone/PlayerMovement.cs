41606697d4fec807cc372ec85d25197a256468b8
DarkTiger <darktigerdevelop@gmail.com>
2017-07-02 19:26:12 +0200

    Player Movement Script Refactoring

--- Assets/Scripts/Simone/PlayerMovement.cs
+++ Assets/Scripts/Simone/PlayerMovement.cs
@@ -2,87 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
-    
-    public GameObject character;
-	public float speed = 8f;
-    public float rotSpeed = 210f;
-    private bool isGround;
-    private bool isWall;
-    
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed;
+    public float rotSpeed;
+    public float jumpSpeed;
+    private bool onGround;
     private Rigidbody rb;
+
 
 	void Start ()
     {
 		rb = gameObject.GetComponent<Rigidbody>();
 	}
 
-	void Update () 
 
+	void Update () 
 	{
-        // Move RIGHT
-        if (Input.GetKey(KeyCode.D) && isWall == false)
+        if (Input.GetKey(KeyCode.D))
         {
-            //transform.Rotate(Vector3.up * rotSpeed * Time.deltaTime);
-            rb.AddTorque(Vector3.up * speed * Time.deltaTime);
+            transform.Rotate(Vector3.up * rotSpeed * Time.deltaTime);
         }
 
         // Move LEFT
-        if (Input.GetKey(KeyCode.A) && isWall == false)
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(-Vector3.up * rotSpeed * Time.deltaTime);
         }
 
         // Move FOWARD
-        if (Input.GetKey(KeyCode.W) && isWall == false)
+        if (Input.GetKey(KeyCode.W))
         {
-            //transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            //rb.AddForce(Vector3.forward * speed * Time.deltaTime);
-            rb.AddRelativeForce(transform.forward * speed, ForceMode.Acceleration);//new Vector3(0, 0, speed);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
         // Move BACK
-        if (Input.GetKey(KeyCode.S) && isWall == false)
+        if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
 
-        // JUMP
-        if (Input.GetButtonDown("Jump") && isGround == true)
+        if (Input.GetKey(KeyCode.Space))
         {
-            isGround = false;
-            rb.velocity = new Vector3(0, 8f, 0);
-
+            if (onGround)
+            {
+                rb.AddExplosionForce(jumpSpeed, transform.position, 1);
+                onGround = false;
+            }
         }
-	}
+    }
 
     // Jumps only if you are on the ground
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Resource"))
+        if (other.collider.tag == "Ground" || other.collider.tag == "Resource")
         {
-            isGround = true;
+            onGround = true;
         }
-
-        /*if (other.gameObject.CompareTag("Solid"))
-        {
-            isWall = true;
-        }
-
-        else
-        {
-            isWall = false;
-        }*/
     }
-        
-    void OnCollisionExit(Collision other)
-    {
-        //isWall = false;
-    }
-
-
-    /*void OnCollisionEnter(Collision other)
-    {
-        this.transform.position.y = 1;
-    }*/
 }
