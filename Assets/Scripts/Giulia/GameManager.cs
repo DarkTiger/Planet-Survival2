3d8d0fbacf89ad11c84d9dba4ba32dc2697ce4ad
DarkTiger <darktigerdevelop@gmail.com>
2017-07-06 12:17:09 +0200

    Morte player tragica

---
+++ Assets/Scripts/Giulia/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject getConditions;
+    PlayerConditions playerConditions;
+    GameObject player;
+
+
+    void Start()
+    {
+        playerConditions = getConditions.GetComponent<PlayerConditions>();
+        player = GameObject.Find("Player");
+
+        GameOver();
+    }
+
+    void Update()
+    {
+        float healthTemp = playerConditions.health;
+
+        if (healthTemp <= 0.0f)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        GameObject animation = player.transform.GetChild(1).gameObject;
+        animation.GetComponent<Animator>().enabled = false;
+
+        GameObject camera = player.transform.GetChild(0).gameObject;
+        camera.transform.parent = null;
+
+        Rigidbody rbPlayer = player.GetComponent<Rigidbody>();
+        rbPlayer.constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+
+}
